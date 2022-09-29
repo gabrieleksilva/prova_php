e535ab7e44fb395e7215f4df5586059e2127d6d6
Michelle <michelledantasluiz@gmail.com>
2022-09-29 20:12:06 -0300

    Adicionando carrossel e uma div lateral

--- principal.php
+++ principal.php
@@ -18,7 +18,9 @@ if (file_exists($nomeArquivo)) {
 <head>
     <meta charset="utf-8">
     <title>Pet shop</title>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
 
     <style>
@@ -37,13 +39,56 @@ if (file_exists($nomeArquivo)) {
 
 <body>
     <div class="container">
+        <header>
+            <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img class="d-block w-100" src="imagens_php/animais.jpg" alt="First slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="imagens_php/passaros.jpg" alt="Second slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="imagens_php/coelho.jpg" alt="Third slide">
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+        </header>
         <div class="row">
-            <div class="col-2">
-                
-                <img src="imagens_php/img-1.jpg" height="300" width="550">
+
+            <div class="col-1">
+
+                <!-- <img src="imagens_php/img-1.jpg" height="300" width="550"> -->
+            </div>
+            <div class="col-2 mb-2 bg-secondary text-white">
+                <h5>Aqui você encontra:</h5>
+                <ul class="list-group">
+
+                    <li class="list-group-item active">Acessórios</li>
+                    <li class="list-group-item active">Brinquedos</li>
+                    <li class="list-group-item active">Camas</li>
+                    <li class="list-group-item active">Casinhas</li>
+                    <li class="list-group-item active">Farmácia</li>
+                    <li class="list-group-item active">Higiene</li>
+                </ul>
+                <h4>Nossos objetivos</h4>
+                <ul>
+                    <li>Ser referência de serviços e inovação em Pet Shop.</li>
+                    <li>Trabalharemos sempre para manter seu pet feliz e saudável</li>
+                    <li>Profissionais altamente qualificados para melhor atender seu pet</li>
+
+                </ul>
             </div>
             <div class="col-8">
-                
+
                 <div>
                     <header>
                         <h1 class="text-center">PetShop</h1><br>
@@ -54,20 +99,20 @@ if (file_exists($nomeArquivo)) {
                 <form action="processamento.php" method="REQUEST">
                     <div class="form-group">
                         <label for="nomeCliente">Nome do cliente:</label>
-                        <input type="text" class="form-control" name="txtNomeCliente" placeholder="Digite o seu nome.." >
+                        <input type="text" class="form-control" name="txtNomeCliente" placeholder="Digite o seu nome..">
 
                     </div>
                     <div class="form-group">
                         <label for="telefone">Telefone:</label>
-                        <input class="form-control" type="text" name="txtTelefone" placeholder="Digite o telefone.." >
+                        <input class="form-control" type="text" name="txtTelefone" placeholder="Digite o telefone..">
                     </div>
                     <div class="form-group">
                         <label for="nomeAnimal">Nome do animal:</label>
-                        <input type="text" class="form-control" name="txtNomeAnimal" placeholder="Digite o nome do animal.." >
+                        <input type="text" class="form-control" name="txtNomeAnimal" placeholder="Digite o nome do animal..">
                     </div>
                     <div class="form-group">
                         <label for="idadeAnimal">Idade do animal:</label>
-                        <input type="number" class="form-control" name="numIdadeAnimal" placeholder="Digite a idade.." >
+                        <input type="number" class="form-control" name="numIdadeAnimal" placeholder="Digite a idade..">
                     </div>
                     <div class="form-group">
                         <label for="atendimento">Atendimento de interesse</label>
@@ -117,29 +162,28 @@ if (file_exists($nomeArquivo)) {
                     </tr>
 
                     <?php
-                        if (!empty($petShop)) {
-                            //$petShopKey essa váriavel que me passa o índice
-                            foreach ($petShop as $petShopKey => $petShopValor) {
-                                echo "<tr>";
-                                //echo "<td>$petShopKey</td> ";
-                                echo "   <td>" . $petShopValor['NomeCliente'] . "</td> ";
-                                echo "   <td>" . $petShopValor['Telefone'] . "</td> ";
-                                echo "   <td>" . $petShopValor['NomeAnimal'] . "</td> ";
-                                echo "   <td>" . $petShopValor['NumIdadeAnimal'] . "</td> ";
-                                echo "   <td>" . $petShopValor['Atendimento'] . "</td> ";
-                                echo "   <td>" . $petShopValor['Pet'] . "</td> ";
-                                echo "   <td>" . $petShopValor['SugestoesReclamacoes'] . "</td> ";
-                                //passando dados para outra página ? nessa estrutura chave/valor
-                                echo "   <td><a href='alterar.php?indice=$petShopKey'>Alterar</a></td> ";
-                                echo "   <td><a href='excluir.php?indice=$petShopKey'>Excluir</a>-</td> ";
-                                echo "</tr>";
-                            }
+                    if (!empty($petShop)) {
+                        //$petShopKey essa váriavel que me passa o índice
+                        foreach ($petShop as $petShopKey => $petShopValor) {
+                            echo "<tr>";
+                            //echo "<td>$petShopKey</td> ";
+                            echo "   <td>" . $petShopValor['NomeCliente'] . "</td> ";
+                            echo "   <td>" . $petShopValor['Telefone'] . "</td> ";
+                            echo "   <td>" . $petShopValor['NomeAnimal'] . "</td> ";
+                            echo "   <td>" . $petShopValor['NumIdadeAnimal'] . "</td> ";
+                            echo "   <td>" . $petShopValor['Atendimento'] . "</td> ";
+                            echo "   <td>" . $petShopValor['Pet'] . "</td> ";
+                            echo "   <td>" . $petShopValor['SugestoesReclamacoes'] . "</td> ";
+                            //passando dados para outra página ? nessa estrutura chave/valor
+                            echo "   <td><a href='alterar.php?indice=$petShopKey'>Alterar</a></td> ";
+                            echo "   <td><a href='excluir.php?indice=$petShopKey'>Excluir</a>-</td> ";
+                            echo "</tr>";
                         }
+                    }
                     ?>
                 </table>
             </div>
-            <div class="col-2">
-            </div>
+
         </div>
     </div>
 
